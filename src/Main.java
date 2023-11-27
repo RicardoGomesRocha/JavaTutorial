23aae354f0837a77a59b12a286c46a04cbc988fb
= <ricardo1992rocha@gmail.com>
2023-11-27 09:53:21 +0000

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import java.util.Date;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        variables();
+    }
+
+    private static void variables() {
+        int myAge = 30;
+        int hearAge = myAge;
+        System.out.println(hearAge);
+        long longNumber = 123_456_123L;
+        System.out.println(longNumber);
+        float price = 10.99F;
+        System.out.println(price);
+        char letter = 'A';
+        System.out.println(letter);
+        boolean isEligible = true;
+        System.out.println(isEligible);
+        Date now = new Date();
+        System.out.println(now);
     }
 }
