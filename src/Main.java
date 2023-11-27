eac4f422e97154f77fec68800a6f5947737056fc
= <ricardo1992rocha@gmail.com>
2023-11-27 11:03:59 +0000

    User Input

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 import java.text.NumberFormat;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Variables.print();
-        AdvanceConcepts.print();
+        // Variables.print();
+        // AdvanceConcepts.print();
+        userInput();
+    }
+
+    private static void userInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Age: ");
+        byte age = scanner.nextByte();
+        System.out.println("You are " + age + " years old");
+        scanner.nextLine();
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("Your are: " + name);
     }
 }
